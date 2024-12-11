486c73e7d79cdd53912356647ab56751c348c573
Michael Smit <msmit@cosoft.co.za>
2024-12-11 13:49:07 +0200

    Form added

---
+++ Models/FormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TelerikDemo.Models;
+
+public record FormModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+}
