adc92d328af6fe2adc98636ac4d09c14b8a6ec7b
Michael Smit <msmit@cosoft.co.za>
2024-12-11 16:37:30 +0200

    ...

---
+++ Models/PanelItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TelerikDemo.Models;
+
+public class PanelItem
+{
+    public string Text { get; set; }
+    public string Url { get; set; }
+    public string Icon { get; set; }
+    public List<PanelItem> Items { get; set; }
+}
